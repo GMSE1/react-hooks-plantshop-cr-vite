326d72130ab2044898641d81e5fa0bfce1e9028e
GMSE1 <gregory.a.marshall.jr@gmail.com>
2025-12-12 18:16:15 -0500

    Plantsy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import PlantPage from "./PlantPage";
 
 function App() {
   return (
-    <div className="app">
+    <div className="App">
       <Header />
       <PlantPage />
     </div>
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
-import PlantList from "./PlantList";
 import Search from "./Search";
+import PlantList from "./PlantList";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then(res => res.json())
+      .then(data => setPlants(data));
+  }, []);
+
+  const displayedPlants = plants.filter(plant =>
+    plant.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleAddPlant = (newPlant) => {
+    setPlants([...plants, newPlant]);
+  };
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchQuery={searchQuery} onSearchChange={setSearchQuery} />
+      <PlantList plants={displayedPlants} /> {}
     </main>
   );
 }
 
 export default PlantPage;
+
